f4858961c04656c204d67786c6ae6492c7702a29
Keith Campbell <keithcc1966@gmail.com>
2017-05-11 21:10:35 -0400

    update Float and Array{T} to pass 0.6 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,7 @@ P22 = [2. 3]
 @test [2     3] == bezier(1,    P21, P22)
 
 # quadratic interpolation with vector of vectors
-@test Array[[10, 5]] == bezier(0.5, Vector{FloatingPoint}[[0., 0], [10, 10], [20, 0]])
+@test Array[[10, 5]] == bezier(0.5, Vector{Float64}[[0., 0], [10, 10], [20, 0]])
 
 # quadratic interpolation with matrix
 @test [[10 5];] == bezier(0.5, [[0. 0]; [10 10]; [20 0]])
@@ -28,7 +28,7 @@ P22 = [2. 3]
 # boundary checks
 @test_throws DomainError bezier(prevfloat(0.), [0., 1], [2., 3])
 @test_throws DomainError bezier(nextfloat(1.), [0., 1], [2., 3])
-@test_throws DomainError bezier(prevfloat(0.), Vector{FloatingPoint}[[0., 1], [2, 3]])
-@test_throws DomainError bezier(nextfloat(1.), Vector{FloatingPoint}[[0., 1], [2, 3]])
+@test_throws DomainError bezier(prevfloat(0.), Vector{Float64}[[0., 1], [2, 3]])
+@test_throws DomainError bezier(nextfloat(1.), Vector{Float64}[[0., 1], [2, 3]])
 @test_throws DomainError bezier(prevfloat(0.), [0. 1; 2 3])
 @test_throws DomainError bezier(nextfloat(1.), [0. 1; 2 3])
